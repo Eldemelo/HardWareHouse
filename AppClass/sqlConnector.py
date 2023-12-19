fe4654bb8571d2c6c445ca0b78e0ca16a1f9808f
Ethan DeMelo <ethan.demelo2000@gmail.com>
2023-12-18 23:10:56 -0500

    update code

--- AppClass/sqlConnector.py
+++ AppClass/sqlConnector.py
@@ -1,5 +1,13 @@
 import mysql.connector
 
+class databaseAccess():
+    def __init__(self):
+        return
+    
+    def checkForDB():
+        #TODO
+        dbExists = False
+        return dbExists
 
 
 db = mysql.connector.connect(
